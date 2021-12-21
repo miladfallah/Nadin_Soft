fe50832bfd6a82c858d57c53f0a413b30d9e4c1b
Milad Fallah <falahmilad79@gmail.com>
2021-12-21 16:51:02 +0330

    database changed to mysql from mongo

---
+++ models/tables.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require("sequelize");
+
+// const { schema } = require("./secure/userValidation");
+const sequelize = require("../utils/database");
+
+exports.User = sequelize.define("User", {
+    //? Model attributes
+    fullname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+// User.validate = (schema) => async (req, res, next) => {
+//     try {
+//       await schema.validate({
+//         fullname: req.body.fullname,
+//         email: req.body.email,
+//         password: req.body.password,
+//       });
+//       return next();
+//     } catch (err) {
+//       return res.status(500).json({ type: err.name, message: err.message });
+//     }
+//   };
+
+exports.Task = sequelize.define("Task", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+  name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+  },
+
+  priority: {
+    type: DataTypes.ENUM('high', 'mediume', 'low'),
+      allowNull: false,
+  },
+});
