8db10c2525522a0cafd1e2a20b57f4bccf3d3df8
miladfallah <59369921+miladfallah@users.noreply.github.com>
2021-12-28 13:53:22 +0330

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,3 +1,5 @@
+const dotEnv = require("dotenv");
+dotEnv.config({ path: "./config/config.env" });
 
 const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize("mydb", "root", "M4900893714f?" , {
